bd3c5a391efa0a89c16ca92a679f9b974f2129bd
Siddhartha <siddhartha.bhattacharjee@ca.com>
2014-04-16 15:56:26 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Examseries::Application.config.secret_token = '9081eff23186f6d388c3140e9532e76ea8b36d7bc764cf99826d06453766a2f015808a6faada4072f33d615fb8166046bf2b83e4c4e7eb9c8bb33c508e138fa9'
